37d5feaf82bf3ff9a470f7b39ecce27e9ee51399
Mikelona556 <oluwatosinmichael88@gmail.com>
2022-11-10 11:17:48 +0100

    first push

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let body = document.body
+let toggle = document.querySelector('.toggle')
+let icon = document.querySelector('.fa-moon')
+let iconB = document.querySelector('.fa-sun')
+
+toggle.onclick = () =>{
+    body.classList.toggle("dark")
+    if(body.classList.contains('dark')){
+        icon.classList.replace('fa-moon', 'fa-sun');
+    }else{
+        icon.classList.replace('fa-sun', 'fa-moon');
+    }
+}
+
+// let body = document.body
+// let toggle = document.querySelector('.toggle')
+// let icon = document.querySelector(".fa-sun")
+
+// function checkIcon(){
+//     if(body.classList.contains('dark')){
+//         icon.classList.add('fa-moon')
+//         icon.classList.remove('fa-sun')
+//     }else{
+//         icon.classList.add('fa-sun')
+//         icon.classList.remove('fa-moon')
+//     }
+// }
+
+// checkIcon()
+
+// toggle.onclick = () =>{
+//     body.classList.toggle("dark")
+//     setTimeout(()=>{
+//         checkIcon()
+//     },100)
+// }
